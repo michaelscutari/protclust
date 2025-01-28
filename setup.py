a91ca1862265fc54f6c324e3532eb2860cb6b530
Michael Scutari <michael.scutari@duke.edu>
2025-01-28 11:05:37 -0500

    version change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mmseqspy",
-    version="0.1.2",
+    version="0.1.3",
     description="A utility library for clustering sequences and splitting data with MMseqs2 and pandas",
     packages=find_packages(),
     install_requires=[
