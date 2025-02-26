da6c0711031734405174367ef586199c4f83d138
Michael Scutari <michael.scutari@duke.edu>
2025-02-26 12:17:20 -0500

    mmseqspy 2.0!

--- mmseqspy/__init__.py
+++ mmseqspy/__init__.py
@@ -1,3 +1,23 @@
-from .cluster_utils import cluster, clean, train_test_cluster_split, train_test_val_cluster_split, split, set_verbosity
+from .cluster_utils import (
+    clean,
+    split,
+    cluster,
+    train_test_cluster_split,
+    train_test_val_cluster_split,
+    set_verbosity,
+    set_seed,
+    constrained_split,
+    cluster_kfold,
+)
 
-__all__ = ["clean", "split", "cluster", "train_test_cluster_split", "train_test_val_cluster_split", "set_verbosity"]
+__all__ = [
+    "clean",
+    "split",
+    "cluster",
+    "train_test_cluster_split",
+    "train_test_val_cluster_split",
+    "set_verbosity",
+    "set_seed",
+    "constrained_split",
+    "cluster_kfold",
+]
