e58d036be4f3dc05ffe61776d6a138b704e75872
Michael Scutari <michael.scutari@duke.edu>
2025-02-26 12:29:58 -0500

    update setup.py"

--- setup.py
+++ setup.py
@@ -25,10 +25,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
     ],
     keywords="bioinformatics, protein, sequence, clustering, mmseqs2",
 )
